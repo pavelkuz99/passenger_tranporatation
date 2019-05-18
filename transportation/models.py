52a19466fb7bf81c1c3b2c65bddefd5ef14037b9
Pavel Kuzmich <pavel.kuzmich2014@yandex.com>
2019-05-18 16:36:22 +0300

    finished models.py

--- transportation/models.py
+++ transportation/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Route(models.Model):
     start = models.CharField(max_length=15, blank=False, default='')
     end = models.CharField(max_length=15, blank=False, default='')
-    distance = models.IntegerField(max_length=3, default=0)
+    distance = models.IntegerField(default=0)
 
     class Meta:
         ordering = ('start',)
@@ -17,7 +17,7 @@ class Ride(models.Model):
     start = models.CharField(max_length=15, blank=False, default='')
     end = models.CharField(max_length=15, blank=False, default='')
     date_and_time = models.DateTimeField()
-    ride_route = models.ForeignKey(
+    route = models.ForeignKey(
         Route,
         related_name='rides',
         on_delete=models.CASCADE
@@ -34,9 +34,9 @@ class Client(models.Model):
     name = models.CharField(max_length=50, blank=False, default='')
     phone = models.CharField(max_length=13, blank=False, default='', unique=True)
     status = models.CharField(max_length=15, blank=False, default='normal')
-    client_rides = models.ForeignKey(
+    ride = models.ForeignKey(
         Ride,
-        related_name='rides',
+        related_name='clients',
         on_delete=models.CASCADE
     )
 
@@ -47,10 +47,47 @@ class Client(models.Model):
         return f'{self.name}: {self.phone}'
 
 
-class ClientRides(models.Model):
+class ClientRide(models.Model):
     client = models.ForeignKey(
         Client,
         related_name='rides',
         on_delete=models.CASCADE
     )
-    ride = mode
+    ride = models.ForeignKey(
+        Ride,
+        on_delete=models.CASCADE
+    )
+
+
+class Driver(models.Model):
+    name = models.CharField(max_length=50, blank=False, default='')
+    phone = models.CharField(max_length=13, blank=False, default='', unique=True)
+    rating = models.IntegerField(blank=False, default=0)
+    ride = models.ForeignKey(
+        Ride,
+        related_name='driver',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return f'{self.name}: {self.phone}'
+
+
+class Vehicle(models.Model):
+    car_license = models.CharField(max_length=7, blank=False, default='')
+    car_model = models.Model(max_length=15, blank=False, default='')
+    color = models.CharField(max_length=15, blank=False, default='white')
+    ride = models.ForeignKey(
+        Ride,
+        related_name='vehicle',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ('car_license',)
+
+    def __str__(self):
+        return f'{self.car_license}:{self.car_model}:{self.color}'
